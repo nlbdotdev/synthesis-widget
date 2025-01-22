d040a8a2d4dfb0bb223168cff40c1506c6f5b3c7
Nathan Bennett <ReflextionsDev@gmail.com>
2025-01-21 21:12:14 -0500

    remove playbutton

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,6 +1,5 @@
 import { Dispatch, SetStateAction, useState, useEffect } from 'react'
 import { WidgetState } from './Widget'
-import { PlayButton } from './PlayButton'
 import { motion, AnimatePresence } from 'framer-motion'
 
 interface ControlPanelProps {
@@ -9,6 +8,34 @@ interface ControlPanelProps {
   playComparatorAnimation: () => void
   resetComparison: () => void
 }
+
+// Play button component merged inline
+const PlayButton = ({
+  className = '',
+  isReset = false,
+}: {
+  className?: string
+  isReset?: boolean
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="48"
+    height="48"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    className={className}
+  >
+    {isReset ? (
+      <path d="M3 12a9 9 0 1 0 9-9 9.75 9.75 0 0 0-6.74 2.74L3 8" />
+    ) : (
+      <polygon points="5 3 19 12 5 21" />
+    )}
+  </svg>
+)
 
 const ControlPanel = ({
   state,
