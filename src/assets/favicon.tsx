0cb00a7dee6fc14b9a0cc5c1e071360892f10ec7
Nathan Bennett <ReflextionsDev@gmail.com>
2025-01-21 22:40:58 -0500

    update favicon & title

---
+++ src/assets/favicon.tsx
@@ -0,0 +1,16 @@
+export const FaviconSvg = () => (
+  <svg
+    height="58"
+    width="58"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 58 58"
+    fill="#000000"
+  >
+    <g>
+      <polygon fill="#b7f4f0" points="29,58 3,45 3,13 29,26" />
+      <polygon fill="#117bbc" points="29,58 55,45 55,13 29,26" />
+      <polygon fill="#5ed2e5" points="3,13 28,0 55,13 29,26" />
+    </g>
+  </svg>
+)
